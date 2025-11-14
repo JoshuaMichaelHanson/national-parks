852ae00faaa7ad073caf521c1d21a1a082312326
josh <josh@hansonslogic.com>
2025-11-14 05:37:18 -0600

    feat(Angular Router) nginx.conf Added a nixpacks.toml and nginx.conf file to fix the deep links from showing 404 during a page refresh or going to any page that is not the homepage directly

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     port: 3000,
-    host: true
+    host: true,
+    strictPort: true,
+    open: true
+  },
+  server: {
+    historyApiFallback: true
   },
   test: {
     globals: true,
